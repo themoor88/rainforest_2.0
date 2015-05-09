d2abd98c2eac82d61b626f6961bb55929c7c647d
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-09 18:24:24 -0400

    Added authorization and added a review form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,10 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
+    if current_user
+      @review = @product.reviews.build
+    end
   end
 
   def new
@@ -41,6 +45,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :price_in_cents)
+    params.require(:product).permit[:name, :description, :price_in_cents]
   end
 end
