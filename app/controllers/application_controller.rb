47d6bca649e1c6bce5052672afbeb1bc2fd613ea
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-09 16:48:59 -0400

    Added 'Logged In' and 'Signed in as' functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # A private helper method that helps identify the current user logged in, if it exists.
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
 end
