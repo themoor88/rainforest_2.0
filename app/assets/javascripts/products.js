bf8c15fd523cde270a1f85dd1dd9fc3b3464d37e
Wajid Kagzi <wjkagzi@gmail.com>
2015-05-10 09:51:18 -0400

    AJAXified the Search form.

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,34 @@
+$(document).on('ready page:load', function() {
+  // function displaySearchResults() {
+  //   if (this.readyState === 4) {
+  //     console.log(this);
+  //     document.getElementById('products').innerHTML = this.responseText;
+  //   }
+  // }
+
+  // var form = document.getElementById('search-form');
+  // form.addEventListener('submit', function(event){
+  //   event.preventDefault();
+  //   var searchValue = document.getElementById('search').value;
+
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.onload = displaySearchResults;
+  //   xhr.open('GET', '/products?search=' + searchValue, true);
+  //   xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  //   xhr.send();
+  // });
+
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'script'
+    }).done(function(data) {
+      $('#products').html(data);
+    });
+
+  });
+});
